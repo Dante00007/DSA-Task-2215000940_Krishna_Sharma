7155bd943c6a83f1daf7923f5574b62becb1fca1
Krishna Sharma <155812881+Dante00007@users.noreply.github.com>
2024-12-22 20:09:41 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (6.21%) - LeetSync

--- 69-sqrtx/sqrtx.cpp
+++ 69-sqrtx/sqrtx.cpp
@@ -1,27 +1,20 @@
 class Solution {
 public:
-    long long int binarySearch(int x){
+    int mySqrt(int x) {
         int s = 0;
         int e = x/2+1;
         long long int ans =-1;
         long long int mid = s +(e-s)/2;
-        long long int sqr = mid*mid ;      
+        long long int sqr = mid*mid ;  
         while(s<=e){
-            long long int sqr = mid*mid;  
-            if(sqr == x)
-                return mid;
-            if(sqr<x){
+            sqr = mid*mid;
+            if(sqr<=x){
                 ans = mid;
                 s = mid+1;
             }
-            else
-                e = mid -1;    
-            mid = s + (e-s)/2;
+            else e = mid -1;
+            mid = s + (e -s)/2;
         }
         return ans;
-}
-    int mySqrt(int x) {
-        return binarySearch(x);
-        
     }
 };

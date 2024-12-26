81550ac7dfd186d454d7265c6fc1991618bb7db3
Krishna Sharma <155812881+Dante00007@users.noreply.github.com>
2024-12-26 15:57:14 +0530

    Time: 62 ms (12.62%) | Memory: 31.1 MB (8.57%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -1,24 +1,24 @@
 class Solution {
 public:
     void bfs(int row,int col,vector<vector<char>> &grid,vector<vector<int>> &visited){
-        int n = grid.size();
-        int m = grid[0].size();
-        visited[row][col] = 1;
+        int m = grid.size();
+        int n = grid[0].size();
         queue<pair<int,int>> q;
         q.push({row,col});
         while(!q.empty()){
-            int row = q.front().first;
-            int col  = q.front().second;
+            row = q.front().first;
+            col  = q.front().second;
             q.pop();
 
+            if(visited[row][col]==1) continue;
+            visited[row][col] = 1;
             vector<int> rowChoice{-1,0,1,0};
             vector<int> colChoice{0,-1,0,1};
             for(int i=0;i<4;i++){
                 int nbrRow = row + rowChoice[i];
                 int nbrCol = col + colChoice[i];
-                if(nbrRow>=0 && nbrCol>=0 && nbrRow<n && nbrCol<m &&
+                if(nbrRow>=0 && nbrCol>=0 && nbrRow<m && nbrCol<n &&
                      !visited[nbrRow][nbrCol] && grid[nbrRow][nbrCol]=='1'){
-                        visited[nbrRow][nbrCol] = 1;
                         q.push({nbrRow,nbrCol});
                 }  
             }
@@ -27,13 +27,13 @@ public:
     }
     int numIslands(vector<vector<char>>& grid) {
         
-        int n = grid.size();
-        int m = grid[0].size();
-        vector<vector<int>> visited(n,vector<int>(m,0));
+        int m = grid.size();
+        int n = grid[0].size();
+        vector<vector<int>> visited(m,vector<int>(n,0));
         
         int count = 0;
-        for(int i = 0;i<n;i++){
-            for(int j = 0;j<m;j++){
+        for(int i = 0;i<m;i++){
+            for(int j = 0;j<n;j++){
                 if(grid[i][j]=='1' && visited[i][j]==0){
                     bfs(i,j,grid,visited);
                     count++;

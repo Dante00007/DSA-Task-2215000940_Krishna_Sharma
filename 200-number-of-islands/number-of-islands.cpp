804d943a7c5ddf43bc3eed886176cf1ede6c6ec6
Krishna Sharma <155812881+Dante00007@users.noreply.github.com>
2024-12-26 15:54:23 +0530

    Time: 51 ms (14.57%) | Memory: 29.1 MB (11.86%) - LeetSync

---
+++ 200-number-of-islands/number-of-islands.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    void bfs(int row,int col,vector<vector<char>> &grid,vector<vector<int>> &visited){
+        int n = grid.size();
+        int m = grid[0].size();
+        visited[row][col] = 1;
+        queue<pair<int,int>> q;
+        q.push({row,col});
+        while(!q.empty()){
+            int row = q.front().first;
+            int col  = q.front().second;
+            q.pop();
+
+            vector<pair<int, int>> directions = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+            for (auto [dr, dc] : directions){
+                int nbrRow = row + dr;
+                int nbrCol = col + dc;
+                if(nbrRow>=0 && nbrCol>=0 && nbrRow<n && nbrCol<m &&
+                     !visited[nbrRow][nbrCol] && grid[nbrRow][nbrCol]=='1'){
+                        visited[nbrRow][nbrCol] = 1;
+                        q.push({nbrRow,nbrCol});
+                }  
+            }
+
+        }
+    }
+    int numIslands(vector<vector<char>>& grid) {
+        
+        int n = grid.size();
+        int m = grid[0].size();
+        vector<vector<int>> visited(n,vector<int>(m,0));
+        
+        int count = 0;
+        for(int i = 0;i<n;i++){
+            for(int j = 0;j<m;j++){
+                if(grid[i][j]=='1' && visited[i][j]==0){
+                    bfs(i,j,grid,visited);
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+};

876436a351bc9522510a16b17480c9a276affdbb
Krishna Sharma <155812881+Dante00007@users.noreply.github.com>
2024-12-27 17:31:32 +0530

    Time: 1 ms (22.07%) | Memory: 17.1 MB (45.61%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.cpp
+++ 801-is-graph-bipartite/is-graph-bipartite.cpp
@@ -19,12 +19,29 @@ public:
         }
         return true;
     }
+    bool dfs(vector<vector<int>>& graph,vector<int> &color,int node,int col){
+        color[node] = col;
+        for(auto nbr:graph[node]){
+            if(color[nbr]==-1){
+                if(!dfs(graph,color,nbr,!col)) return false;;
+            }
+            else if(color[node]==color[nbr]){
+                return false;
+            }
+        }
+        return true;
+    }       
     bool isBipartite(vector<vector<int>>& graph) {
         int n = graph.size();
         vector<int> color(n,-1);
+        // for(int i = 0;i<n;i++){
+        //     if(color[i]==-1){
+        //         if(!bfs(graph,color,i)) return false;
+        //     }
+        // }
         for(int i = 0;i<n;i++){
             if(color[i]==-1){
-                if(!bfs(graph,color,i)) return false;
+                if(!dfs(graph,color,i,0)) return false;
             }
         }
         return true;

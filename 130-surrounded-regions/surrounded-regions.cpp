b50154bac84148eb6fecb091b03bcc4e78d0a23e
Krishna Sharma <155812881+Dante00007@users.noreply.github.com>
2024-12-27 16:05:41 +0530

    Time: 2 ms (31.91%) | Memory: 15.9 MB (15.05%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.cpp
+++ 130-surrounded-regions/surrounded-regions.cpp
@@ -18,14 +18,26 @@ public:
         int m = board.size();
         int n = board[0].size();
         vector<vector<int>> vis(m,vector<int> (n,0));
-        for(int i = 0;i<m;i++){
-            for(int j = 0;j<n;j++){
-                if(board[i][j]=='O' && (i==0 || i==m-1 || j==n-1 || j==0) && 
-                    vis[i][j]==0) {
-                    dfs(board,vis,i,j);
-                }
+
+        for(int j = 0 ; j<n;j++) { 
+            if(!vis[0][j] && board[0][j] == 'O') {
+                dfs( board, vis, 0, j); 
+            }
+            if(!vis[m-1][j] && board[m-1][j] == 'O') {
+                dfs(board, vis,m-1,j); 
             }
         }
+        
+        for(int i = 0;i<m;i++) {
+            if(!vis[i][0] && board[i][0] == 'O') {
+                dfs(board,vis,i,0); 
+            }
+            
+            if(!vis[i][n-1] && board[i][n-1] == 'O') {
+                dfs(board,vis,i,n-1); 
+            }
+        }
+        
         for(int i = 0;i<m;i++){
             for(int j = 0;j<n;j++){
                 if(board[i][j]=='O' && !vis[i][j]){

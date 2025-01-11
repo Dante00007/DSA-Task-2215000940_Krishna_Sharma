4298f3247ebc77c4d8ed4190788d0967b3d3dec8
Krishna Sharma <155812881+Dante00007@users.noreply.github.com>
2025-01-11 17:35:07 +0530

    Time: 44 ms (60.34%) | Memory: 66.1 MB (36.14%) - LeetSync

--- 820-find-eventual-safe-states/find-eventual-safe-states.cpp
+++ 820-find-eventual-safe-states/find-eventual-safe-states.cpp
@@ -1,33 +1,33 @@
 class Solution {
 public:
-    bool dfs(vector<vector<int>>& graph,vector<int> &vis,vector<int> &pathVis,int node,vector<int> & check){
-        vis[node] = 1;
-        pathVis[node] = 1;
-
-        for(auto nbr:graph[node]){
-            if(!vis[nbr]){
-                if(dfs(graph,vis,pathVis,nbr,check)) return true;
-            }
-            else if(pathVis[nbr]){
-                return true;
-            }
-        }
-        pathVis[node] = 0;
-        check[node] = 1;
-        return false;
-    }
+    
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
         int n = graph.size();
-        vector<int> vis(n,0);
-        vector<int> pathVis(n,0);
-        vector<int> check(n,0);
-        vector<int> ans;
+        vector<int> indegree(n,0);
+        // we need this because every outdegree equal to 0 is safe node
+        // but in topo sort we have everything in indegree so we just reverse the list
+        vector<vector<int>> adjRev(n);
         for(int i = 0;i<n;i++){
-            dfs(graph,vis,pathVis,i,check);
+            for(auto it:graph[i]){
+                adjRev[it].push_back(i);
+                indegree[i]++;
+            }
+        }
+        
+        queue<int> q;
+        for(int i = 0;i<n;i++){
+            if(indegree[i]==0) q.push(i);
         }
 
-        for(int i = 0;i<n;i++){
-            if(check[i]) ans.push_back(i);
+        vector<int> ans;
+        while(!q.empty()){
+            int node = q.front();
+            q.pop();
+            ans.push_back(node);
+            for(auto nbr:adjRev[node]){
+                indegree[nbr]--;
+                if(indegree[nbr]==0) q.push(nbr);
+            }
         }
 
         sort(ans.begin(),ans.end());

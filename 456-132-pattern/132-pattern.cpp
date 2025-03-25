ed75681edea626ee9c06f6355b739d2440c7419c
Krishna Sharma <155812881+Dante00007@users.noreply.github.com>
2025-03-25 16:29:19 +0530

    Time: 15 ms (32.32%) | Memory: 70.9 MB (23.05%) - LeetSync

--- 456-132-pattern/132-pattern.cpp
+++ 456-132-pattern/132-pattern.cpp
@@ -5,20 +5,12 @@ public:
         
         stack<int> s;
         int k = INT_MIN;
-        vector<int> min_left(nums.size());
-        min_left[0] = nums[0];
-        int i = 1;
-        while(i<nums.size()){
-            min_left[i] = min(min_left[i-1],nums[i]);
-            i++;
-        }
         for(int i=nums.size()-1;i>=0;i--){
             if(nums[i]<k){
                 return true;
             }
             while(!s.empty() && s.top()<nums[i]){
-                k = s.top();
-                if(k>min_left[i]) return true;
+                k = max(k,s.top());
                 s.pop();
             }
             s.push(nums[i]);

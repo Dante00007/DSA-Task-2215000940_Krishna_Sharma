66f62d7238ff9c8e1e5c561ac31292c9c94e8fe8
Krishna Sharma <155812881+Dante00007@users.noreply.github.com>
2025-04-23 15:50:13 +0530

    Time: 1 ms (41.85%) | Memory: 17.3 MB (19.44%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.cpp
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -1,25 +1,24 @@
 class Solution {
 public:
-    int bfs(vector<vector<int>>& grid,vector<vector<int>>& vis,queue<pair<pair<int,int>,int>> &q){
+    int bfs(vector<vector<int>>& vis,queue<pair<pair<int,int>,int>> &q,vector<vector<int>>& grid){
         int m = grid.size();
         int n = grid[0].size();
-        
         int minute = 0;
+        vector<pair<int,int>> dir{{0,-1},{0,1},{-1,0},{1,0}};
         while(!q.empty()){
-            int row = q.front().first.first;
-            int col = q.front().first.second;
-            int t = q.front().second;
-            minute = max(minute,t);
+            auto [row,col] = q.front().first;
+            int time = q.front().second;
             q.pop();
-            vector<pair<int,int>> dir{{0,-1},{0,1},{-1,0},{1,0}};
+            minute = max(minute,time);
             for(auto [dr,dc]:dir){
                 int nbrRow = row + dr;
                 int nbrCol = col + dc;
                 if(nbrRow>=0 && nbrCol>=0 && nbrRow<m && nbrCol<n &&
-                 vis[nbrRow][nbrCol]!=2 && grid[nbrRow][nbrCol]==1){
-                    q.push({{nbrRow,nbrCol},t+1});
+                    vis[nbrRow][nbrCol]!=2 && grid[nbrRow][nbrCol]==1
+                ){
+                    q.push({{nbrRow,nbrCol},time+1});
                     vis[nbrRow][nbrCol] = 2;
-                 }
+                }
             }
         }
         return minute;
@@ -37,8 +36,8 @@ public:
                 }
             }
         }
-        int time = 0;
-        time = bfs(grid,vis,q);
+
+        int time = bfs(vis,q,grid);
         for(int i=0;i<m;i++){
             for(int j = 0;j<n;j++){
                 if(vis[i][j]!=2 && grid[i][j]==1){

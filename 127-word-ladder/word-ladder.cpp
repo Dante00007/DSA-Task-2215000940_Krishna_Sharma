2df9095e89b47656c6bb78bdd5f364a3facf2f32
Krishna Sharma <155812881+Dante00007@users.noreply.github.com>
2025-04-23 21:44:53 +0530

    Time: 330 ms (33.30%) | Memory: 21.2 MB (62.50%) - LeetSync

--- 127-word-ladder/word-ladder.cpp
+++ 127-word-ladder/word-ladder.cpp
@@ -6,13 +6,15 @@ public:
             s.insert(wordList[i]);
         }
         queue<pair<string,int>> q;
+        
         q.push({beginWord,1});
         while(!q.empty()){
             string str = q.front().first;
             int steps = q.front().second;
-            if(str==endWord)
-                return steps;
             q.pop();
+            if(str==endWord){
+                return steps;
+            }
             for(int i=0;i<str.size();i++){
                 char orig = str[i];
                 for(char ch= 'a';ch<='z';ch++){

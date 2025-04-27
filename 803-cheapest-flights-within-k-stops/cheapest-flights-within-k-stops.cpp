fb3d186296305bab3859ea19d9ffe19288b9e11b
Krishna Sharma <155812881+Dante00007@users.noreply.github.com>
2025-04-27 20:02:25 +0530

    Time: 7 ms (28.73%) | Memory: 18.4 MB (36.34%) - LeetSync

--- 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
@@ -5,30 +5,23 @@ public:
         for (auto it : flights){
             adj[it[0]].push_back({it[1], it[2]});
         }
-        queue< pair<int,pair<int,int>> > q;
         vector<int> dist(n,1e9);
-        dist[src] = 0;
+        queue<pair<int,pair<int,int>>> q;
         q.push({0,{src,0}});
         while(!q.empty()){
-            int stop = q.front().first;
+            int cost = q.front().first;
             int node = q.front().second.first;
-            int cost = q.front().second.second;
+            int stop = q.front().second.second;
             q.pop();
-
             if(stop>k) continue;
-
-            for(auto it:adj[node]){
-                int nuNode = it.first;
-                int nuWt = it.second;
-                if(cost+nuWt<dist[nuNode]){ 
-                    dist[nuNode] =  cost+nuWt; 
-                    q.push({stop+1,{nuNode,dist[nuNode]}});
+            for(auto [nbr,nbrCost]:adj[node]){
+                if(nbrCost+cost<dist[nbr]){
+                    dist[nbr] = nbrCost+cost;
+                    q.push({dist[nbr],{nbr,stop+1}});
                 }
             }
-
         }
-        if (dist[dst] == 1e9)
-            return -1;
+        if(dist[dst]==1e9) return -1;
         return dist[dst];
     }
 };

c5b304c6475e725131486bdae82a6840757cc9bc
Krishna Sharma <155812881+Dante00007@users.noreply.github.com>
2025-05-04 18:40:10 +0530

    Time: 0 ms (100.00%) | Memory: 13.3 MB (10.77%) - LeetSync

--- 120-triangle/triangle.cpp
+++ 120-triangle/triangle.cpp
@@ -13,10 +13,35 @@ public:
     }
     int minimumTotal(vector<vector<int>>& triangle) {
         int m = triangle.size();
+        // vector<vector<int>> dp(m);
+        // for(int i=0;i<m;i++){
+        //     dp[i] = vector<int> (triangle[i].size(),-1);
+        // }
+        // return solve(triangle,0,0,dp);
+
         vector<vector<int>> dp(m);
         for(int i=0;i<m;i++){
-            dp[i] = vector<int> (triangle[i].size(),-1);
+            dp[i] = vector<int> (triangle[i].size(),0);
         }
-        return solve(triangle,0,0,dp);
+        dp[0][0] = triangle[0][0];
+        for(int i=1;i<m;i++){
+            dp[i][0] = triangle[i][0] + dp[i-1][0];
+        }
+
+        for(int i=1;i<m;i++){
+            for(int j =1;j<triangle[i].size();j++){
+                int sum = triangle[i][j];
+                if(j==i){
+                    dp[i][j] = sum+dp[i-1][j-1];
+                }else{
+                    dp[i][j] = sum + min(dp[i-1][j-1],dp[i-1][j]);
+                }
+            }
+        }
+        int maxi = INT_MAX;
+        for(int i = 0;i<triangle[m-1].size();i++){
+            maxi = min(maxi,dp[m-1][i]);
+        }
+        return maxi;
     }
 };

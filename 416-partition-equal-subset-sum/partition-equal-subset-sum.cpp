51a61ff7ede2eb7dc55585351e57297739fff7b0
Krishna Sharma <155812881+Dante00007@users.noreply.github.com>
2025-05-05 13:02:14 +0530

    Time: 169 ms (21.33%) | Memory: 110.4 MB (16.24%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
-    bool solve(vector<int>& nums,int target,int idx,int sum,vector<vector<int>> &dp){
-        if(target/2==sum) return true;
-        if(idx>=nums.size() || sum>target/2) return false;
-        
+    bool solve(vector<int>& nums,int sum,int idx,vector<vector<int>> &dp){
+        if(sum==0) return true;
+        if(idx>=nums.size() || sum<0) return false;
+
         if(dp[idx][sum]!=-1) return dp[idx][sum];
 
-        int inc = solve(nums,target,idx+1,sum+nums[idx],dp);
-        int exc = solve(nums,target,idx+1,sum,dp);
+        int inc = solve(nums,sum-nums[idx],idx+1,dp);
+        int exc = solve(nums,sum,idx+1,dp);
 
         return dp[idx][sum]= inc || exc;
     }
@@ -19,6 +19,8 @@ public:
         }
         if(target&1) return false;
         vector<vector<int> > dp(n,vector<int> ((target/2)+1 ,-1));
-        return solve(nums,target,0,0,dp);
+        return solve(nums,target/2,0,dp);
+
+        // vector<vector<int> > dp(n,vector<int> ((target/2)+1 ,-1));
     }
 };

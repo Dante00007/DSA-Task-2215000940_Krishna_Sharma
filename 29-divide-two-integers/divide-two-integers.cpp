b39c5d38dbf3e5f2e05bad66f84c857b3841dd57
Krishna Sharma <155812881+Dante00007@users.noreply.github.com>
2025-05-28 15:05:29 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (37.56%) - LeetSync

--- 29-divide-two-integers/divide-two-integers.cpp
+++ 29-divide-two-integers/divide-two-integers.cpp
@@ -3,6 +3,9 @@ public:
     int divide(int dividend, int divisor) {
         if(dividend == INT_MIN && divisor == -1)
 		    return INT_MAX;
+        if(dividend == INT_MIN && divisor == 1)
+		    return INT_MIN;
+        
   
         int sign = ((dividend < 0) ^ (divisor < 0)) ? -1 : 1;
 
